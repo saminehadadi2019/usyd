418d223a9822cbc57374cd075d38aa0ca38c98a3
SamineHadadi <samine.hadadi@gmail.com>
2024-05-15 15:55:55 +1000

    added audience5

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,11 +11,16 @@ module.exports = {
     requireConfigFile: false,
   },
   rules: {
-    // allow reassigning param
+    // Allow reassigning parameters but not their properties
     'no-param-reassign': [2, { props: false }],
     'linebreak-style': ['error', 'unix'],
     'import/extensions': ['error', {
       js: 'always',
     }],
+    'no-undef': 'off'  // This disables the no-undef rule completely
   },
+  globals: {
+    getMetadata: "readonly",
+    getAllMetadata: "readonly"
+  }
 };
