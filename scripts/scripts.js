8fe8a27febd5efc8d99d6a8c8e1c7bd69b1c9a74
SamineHadadi <samine.hadadi@gmail.com>
2024-05-15 20:38:05 +1000

    example added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,282 +1,88 @@
 import {
   sampleRUM,
-  getLanguagePath,
   buildBlock,
   loadHeader,
   loadFooter,
+  decorateButtons,
   decorateIcons,
   decorateSections,
   decorateBlocks,
   decorateTemplateAndTheme,
-  fetchPlaceholders,
   waitForLCP,
   loadBlocks,
-  toClassName,
-  getMetadata,
   loadCSS,
+  getAllMetadata,
+  getMetadata,
+  loadScript,
   toCamelCase,
-} from './lib-franklin.js';
-import TEMPLATE_LIST from '../templates/config.js';
+  toClassName,
+} from './aem.js';
 
+const LCP_BLOCKS = []; // add your LCP blocks to the list
 const AUDIENCES = {
   mobile: () => window.innerWidth < 600,
   desktop: () => window.innerWidth >= 600,
   // define your custom audiences here as needed
 };
 
-/**
- * Gets all the metadata elements that are in the given scope.
- * @param {String} scope The scope/prefix for the metadata
- * @returns an array of HTMLElement nodes that match the given scope
- */
-export function getAllMetadata(scope) {
-  return [...document.head.querySelectorAll(`meta[property^="${scope}:"],meta[name^="${scope}-"]`)]
-    .reduce((res, meta) => {
-      const id = toClassName(meta.name
-        ? meta.name.substring(scope.length + 1)
-        : meta.getAttribute('property').split(':')[1]);
-      res[id] = meta.getAttribute('content');
-      return res;
-    }, {});
-}
+window.hlx.plugins.add('experimentation', {
+  condition: () => getMetadata('experiment')
+    || Object.keys(getAllMetadata('campaign')).length
+    || Object.keys(getAllMetadata('audience')).length,
+  options: { audiences: AUDIENCES },
+  url: '/plugins/experimentation/src/index.js',
+});
 
-// Define an execution context
 const pluginContext = {
   getAllMetadata,
   getMetadata,
   loadCSS,
+  loadScript,
   sampleRUM,
   toCamelCase,
   toClassName,
 };
 
-const LCP_BLOCKS = []; // add your LCP blocks to the list
-window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information here
+// Add you templates below
+// window.hlx.templates.add('/templates/my-template');
+
+// Add you plugins below
+// window.hlx.plugins.add('/plugins/my-plugin.js');
 
 /**
- * Adds chevron to all buttons that are children of element
- * @param element The dom subtree containing buttons
- * @param selector The selector to match buttons
+ * Builds hero block and prepends to main in a new section.
+ * @param {Element} main The container element
  */
-export function addChevronToButtons(element, selector = 'a.button') {
-  /* Add chevron to buttons */
-  element.querySelectorAll(selector).forEach((button) => {
-    const chevron = document.createElement('span');
-    chevron.classList.add('icon', 'icon-chevron-right');
-    button.append(chevron);
-  });
-}
-
-/**
- * decorates paragraphs containing a single link as buttons with classes and
- * chevron icon.
- * @param {Element} element container element
- */
-function decorateButtons(element) {
-  element.querySelectorAll('a').forEach((a) => {
-    if (a.href !== a.textContent) {
-      const up = a.parentElement;
-      const twoup = up.parentElement;
-      const threeUp = twoup.parentElement;
-
-      if (!a.querySelector('img') && !threeUp.classList.contains('fixed-social-media')) {
-        a.title = a.title || a.textContent.trim();
-        if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
-          a.className = 'button'; // default navigational link
-          up.classList.add('button-container');
-        }
-        if (up.childNodes.length === 1 && up.tagName === 'STRONG'
-          && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-          a.className = 'button primary'; // primary CTA button link
-          twoup.classList.add('button-container');
-        }
-        if (up.childNodes.length === 1 && up.tagName === 'EM'
-          && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-          a.className = 'button secondary'; // secondary CTA button link
-          twoup.classList.add('button-container');
-        }
-        addChevronToButtons(up);
-      }
-    }
-  });
-}
-
 function buildHeroBlock(main) {
-  /* 1. If there is an explicit hero block, add it to its own section, so it can be full-width */
-  const heroBlock = main.querySelector('.hero');
-  if (heroBlock) {
-    const section = document.createElement('div');
-    section.append(heroBlock);
-    main.prepend(section);
-    return;
-  }
-
-  /* 2. If we are on a blog post with image, add the image and h1 */
   const h1 = main.querySelector('h1');
-  let picture;
-  let subtitle;
-
-  const h1Sibling = document.querySelector('body.blogpost main h1 + p');
-
-  if (h1Sibling && h1Sibling.firstElementChild?.nodeName === 'PICTURE') {
-    picture = h1Sibling;
-  } else if (h1Sibling && h1Sibling.nextElementSibling?.firstElementChild?.nodeName === 'PICTURE') {
-    picture = h1Sibling.nextElementSibling?.firstElementChild;
-    subtitle = h1Sibling;
-  }
-
-  if (h1 && picture) {
+  const picture = main.querySelector('picture');
+  // eslint-disable-next-line no-bitwise
+  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
-    const hr = document.createElement('hr');
-    section.append(buildBlock('hero', { elems: [hr, h1, subtitle, picture] }));
-    main.prepend(section);
-    return;
-  }
-
-  /* 3. If there is only a h1, build a block out of the h1 */
-  if (h1) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [h1] }));
+    section.append(buildBlock('hero', { elems: [picture, h1] }));
     main.prepend(section);
   }
 }
 
 /**
- * Returns a Franklin icon span (that will be expanded by decorateIcons)
- * @param {string} name The icon file name (minus ".svg")
- * @returns {HTMLSpanElement}
+ * load fonts.css and set a session storage flag
  */
-export function createIcon(name) {
-  const icon = document.createElement('span');
-  icon.classList.add('icon', `icon-${name}`);
-
-  return icon;
-}
-
-function createEmbedIFrame(a, vendor) {
-  const div = document.createElement('div');
-  div.classList.add(`${vendor}-base`);
-  const id = a.pathname.split('/').pop();
-
-  let source;
-  let className;
-  let allow;
-  if (vendor === 'youtube') {
-    source = `https://www.youtube.com/embed/${id}`;
-    className = 'youtube-player';
-    allow = 'encrypted-media; accelerometer; gyroscope; picture-in-picture';
-  } else if (vendor === 'spotify') {
-    source = `https://open.spotify.com/embed/episode/${id}`;
-    className = 'spotify-player';
-    allow = 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture';
-  } else if (vendor === 'wistia') {
-    source = `https://fast.wistia.net/embed/iframe/${id}`;
-    className = 'wistia-player';
-    allow = 'autoplay; clipboard-write; encrypted-media; fullscreen;';
+async function loadFonts() {
+  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
+  try {
+    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
+  } catch (e) {
+    // do nothing
   }
-
-  div.innerHTML = `<iframe data-src="${source}" 
-        class="${className}"
-        allowfullscreen  
-        allow="${allow}"
-        style="display: none"
-        loading="auto">
-    </iframe>`;
-
-  a.replaceWith(div);
-  return div;
-}
-
-function preDecorateEmbed(main) {
-  const lazyEmbeds = [];
-
-  const anchors = main.getElementsByTagName('a');
-  const youTubeAnchors = Array.from(anchors).filter((a) => a.href.includes('youtu') && encodeURI(a.textContent.trim()).indexOf(a.href) !== -1);
-  const spotifyAnchors = Array.from(anchors).filter((a) => a.href.includes('spotify') && encodeURI(a.textContent.trim()).indexOf(a.href) !== -1);
-  const wistiaAnchors = Array.from(anchors).filter((a) => a.href.includes('wistia') && encodeURI(a.textContent.trim()).indexOf(a.href) !== -1);
-
-  youTubeAnchors.forEach((a) => {
-    lazyEmbeds.push(createEmbedIFrame(a, 'youtube'));
-  });
-  spotifyAnchors.forEach((a) => {
-    lazyEmbeds.push(createEmbedIFrame(a, 'spotify'));
-  });
-  wistiaAnchors.forEach((a) => {
-    lazyEmbeds.push(createEmbedIFrame(a, 'wistia'));
-  });
-
-  if ('IntersectionObserver' in window) {
-    // eslint-disable-next-line vars-on-top
-    const lazyEmbedObserver = new IntersectionObserver((entries) => {
-      entries.forEach((embed) => {
-        if (embed.isIntersecting) {
-          const iframe = embed.target.firstChild;
-          iframe.src = iframe.dataset.src;
-          iframe.style.display = iframe.style.display === 'none' ? '' : 'none';
-          lazyEmbedObserver.unobserve(embed.target);
-        }
-      });
-    });
-
-    lazyEmbeds.forEach((lazyEmbed) => {
-      lazyEmbedObserver.observe(lazyEmbed);
-    });
-  } else {
-    lazyEmbeds.forEach((embed) => {
-      const iframe = embed.querySelector('iframe');
-      iframe.src = iframe.dataset.src;
-      iframe.style.display = iframe.style.display === 'none' ? '' : 'none';
-    });
-  }
-}
-
-export function isMarketoFormUrl(url) {
-  return url.hostname === 'engage-lon.marketo.com';
-}
-
-function findParent(element, callback) {
-  if (!element.parentElement) {
-    return null;
-  }
-  if (callback(element.parentElement)) {
-    return element.parentElement;
-  }
-  return findParent(element.parentElement, callback);
-}
-
-function preDecorateMarketoForm(main) {
-  [...main.getElementsByTagName('a')].filter((a) => {
-    try {
-      return a.href
-        && isMarketoFormUrl(new URL(a.href))
-        && !findParent(a, (parent) => parent.classList.contains('form'));
-    } catch (e) {
-      console.error('error while parsing form anchors', e); // eslint-disable-line no-console
-      return false;
-    }
-  }).forEach((a) => {
-    const formDiv = document.createElement('div');
-    formDiv.classList.add('form');
-    formDiv.innerHTML = `<div><div>${a.outerHTML}</div></div>`;
-    a.parentElement.replaceWith(formDiv);
-  });
 }
 
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
  */
-async function buildAutoBlocks(main) {
+function buildAutoBlocks(main) {
   try {
     buildHeroBlock(main);
-    const template = toClassName(getMetadata('template'));
-    const templates = TEMPLATE_LIST;
-    if (templates.includes(template)) {
-      const mod = await import(`../templates/${template}/${template}.js`);
-      if (mod.default) {
-        await mod.default(main);
-      }
-    }
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
@@ -284,145 +90,91 @@ async function buildAutoBlocks(main) {
 }
 
 /**
- * Instruments the main element with document metadata for LiveUX tracking
- * @param {*} main The main element
- */
-function instrumentMain(main) {
-  [...document.head.children]
-    .filter((child) => child.nodeName === 'META' && child.name.startsWith('wp-'))
-    .forEach((meta) => main.setAttribute(`data-${meta.name}`, meta.content));
-}
-
-/**
  * Decorates the main element.
  * @param {Element} main The main element
  */
 // eslint-disable-next-line import/prefer-default-export
-export async function decorateMain(main) {
-  instrumentMain(main);
+export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
-  preDecorateMarketoForm(main);
-  await buildAutoBlocks(main);
+  decorateIcons(main);
+  buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
-  preDecorateEmbed(main);
 }
 
 /**
- * Lazy loads Gellix font from css file
- */
-async function loadGellix() {
-  await loadCSS('/styles/lazy-styles.css');
-}
-
-/**
- * sets the language attribute in the html tag
- */
-function setPageLanguage() {
-  let languagePath = getLanguagePath();
-
-  if (languagePath) {
-    languagePath = languagePath.slice(1);
-  } else {
-    languagePath = 'en';
-  }
-  document.documentElement.lang = languagePath;
-}
-
-/**
- * loads everything needed to get to LCP.
+ * Loads everything needed to get to LCP.
+ * @param {Element} doc The container element
  */
 async function loadEager(doc) {
-  if (getMetadata('experiment')
-    || Object.keys(getAllMetadata('campaign')).length
-    || Object.keys(getAllMetadata('audience')).length) {
-    // eslint-disable-next-line import/no-relative-packages
-    const { loadEager: runEager } = await import('../plugins/experimentation/src/index.js');
-    await runEager(document, { audiences: AUDIENCES }, pluginContext);
-  }
-  setPageLanguage();
+  document.documentElement.lang = 'en';
   decorateTemplateAndTheme();
-  await fetchPlaceholders();
-  loadHeader(doc.querySelector('header'));
+
+  await window.hlx.plugins.run('loadEager', pluginContext);
+
   const main = doc.querySelector('main');
   if (main) {
-    await decorateMain(main);
-    if (document.querySelector('main .section:first-child img')) {
-      await waitForLCP(LCP_BLOCKS);
-    } else {
-      document.querySelector('body').classList.add('appear');
+    decorateMain(main);
+    document.body.classList.add('appear');
+    await waitForLCP(LCP_BLOCKS);
+  }
+
+  try {
+    /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
+    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
+      loadFonts();
     }
-    await loadGellix();
+  } catch (e) {
+    // do nothing
   }
 }
 
 /**
- * Adds the favicon.
- * @param {string} href The favicon URL
- */
-export function addFavIcon(href) {
-  const link = document.createElement('link');
-  link.rel = 'icon';
-  link.href = href;
-  const existingLink = document.querySelector('head link[rel="icon"]');
-  if (existingLink) {
-    existingLink.parentElement.replaceChild(link, existingLink);
-  } else {
-    document.getElementsByTagName('head')[0].appendChild(link);
-  }
-}
-
-/**
- * loads everything that doesn't need to be delayed.
+ * Loads everything that doesn't need to be delayed.
+ * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
   const main = doc.querySelector('main');
   await loadBlocks(main);
-  decorateIcons(main);
 
   const { hash } = window.location;
-  const element = hash ? main.querySelector(hash) : false;
+  const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
+  loadHeader(doc.querySelector('header'));
   loadFooter(doc.querySelector('footer'));
 
-  addFavIcon(`${window.hlx.codeBasePath}/icons/favicon.ico`);
+  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+  loadFonts();
+
+  window.hlx.plugins.run('loadLazy',pluginContext);
+
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
-
-  if ((getMetadata('experiment')
-    || Object.keys(getAllMetadata('campaign')).length
-    || Object.keys(getAllMetadata('audience')).length)) {
-    // eslint-disable-next-line import/no-relative-packages
-    const { loadLazy: runLazy } = await import('../plugins/experimentation/src/index.js');
-    await runLazy(document, { audiences: AUDIENCES }, pluginContext);
-  }
 }
 
 /**
- * loads everything that happens a lot later, without impacting
- * the user experience.
+ * Loads everything that happens a lot later,
+ * without impacting the user experience.
  */
 function loadDelayed() {
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
+  window.setTimeout(() => {
+    window.hlx.plugins.load('delayed');
+    window.hlx.plugins.run('loadDelayed');
+    // eslint-disable-next-line import/no-cycle
+    return import('./delayed.js');
+  }, 3000);
   // load anything that can be postponed to the latest here
 }
 
 async function loadPage() {
+  await window.hlx.plugins.load('eager');
   await loadEager(document);
+  await window.hlx.plugins.load('lazy');
   await loadLazy(document);
   loadDelayed();
 }
 
 loadPage();
-
-const params = new URLSearchParams(window.location.search);
-if (params.get('performance')) {
-  window.hlx.performance = true;
-  import('./performance.js').then((mod) => {
-    if (mod.default) mod.default();
-  });
-}
