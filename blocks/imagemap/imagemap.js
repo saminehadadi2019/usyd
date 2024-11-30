50e64b9d28416cb75a232b6851e75fa4e4affbdd
SamineHadadi <samine.hadadi@gmail.com>
2024-12-01 08:18:03 +1100

    map2

--- blocks/imagemap/imagemap.js
+++ blocks/imagemap/imagemap.js
@@ -2,30 +2,30 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   const text = block.textContent;
- //const text = "src=https://i.ibb.co/S393wpr/map.png;alt=carpet map;shape=circle;coord=200,200,90;alert=area 1;url='https://www.persianrugs.com.au/tabriz/';shape=circle;coord=780,780,140;alert=area 2;url='https://www.persianrugs.com.au/tabriz/'";
+  //const text = "src=https://i.ibb.co/S393wpr/map.png;alt=carpet map;shape=circle;coord=200,200,90;alert=area 1;url='https://www.persianrugs.com.au/tabriz/';shape=circle;coord=780,780,140;alert=area 2;url='https://www.persianrugs.com.au/tabriz/'";
   block.textContent = '';
   text.replace(/\s/g, '').trim;
   const srcRegex = /src=\s*([^;]+);/;
   const altRegex = /alt=\s*([^;]+);/;
-  const shapeRegex = /shape=\s*([^;]+);/g; 
-  const coordRegex = /coord=\s*([^;]+);/g; 
-  const alertRegex = /alert=\s*([^;]+);/g; 
-  const urlRegex = /url=\s*([^;]+);/g; 
-  
+  const shapeRegex = /shape=\s*([^;]+);/g;
+  const coordRegex = /coord=\s*([^;]+);/g;
+  const alertRegex = /alert=\s*([^;]+);/g;
+  const urlRegex = /url=\s*([^;]+);/g;
+
   const srcMatch = text.match(srcRegex);
   const altMatch = text.match(altRegex);
   const shapeMatches = text.match(shapeRegex);
   const coordMatches = text.match(coordRegex);
   const alertMatches = text.match(alertRegex);
   const urlMatches = text.match(urlRegex);
-  
+
   const src = srcMatch ? srcMatch[1].trim() : '';
   const alt = altMatch ? altMatch[1].trim() : '';
   const shapes = shapeMatches ? shapeMatches.map(match => match.trim().replace("shape=", "").replace(";", "")) : [];
   const coords = coordMatches ? coordMatches.map(match => match.trim().replace("coord=", "").replace(";", "")) : [];
   const messages = alertMatches ? alertMatches.map(match => match.trim().replace("alert=", "").replace(";", "")) : [];
   const urls = urlMatches ? urlMatches.map(match => match.trim().replace("url=", "").replace(";", "")) : [];
-  
+
 
   console.log("src:", src);
   console.log("alt:", alt);
@@ -41,23 +41,30 @@ export default function decorate(block) {
   image.alt = alt;
   image.useMap = '#myMap';
 
-  for (let i = 0; i < shapes.length; i++) {
-    const area = document.createElement('area');
-    area.shape = shapes[i];
-    area.coords = coords[i];
-    area.addEventListener('click', () => {
-      if(messages[i])
-        alert(messages[i]);
-    });
-    area.style.borderBlockColor = 'red';
-    area.href = urls[i];
-    area.target ="_blank";
+  try {
+    for (let i = 0; i < shapes.length; i++) {
+      const area = document.createElement('area');
+      area.shape = shapes[i];
+      area.coords = coords[i];
+      area.addEventListener('click', () => {
+        if (messages[i])
+          alert(messages[i]);
+      });
+      area.style.borderBlockColor = 'red';
+      area.href = urls[i];
+      area.target = "_blank";
 
-    map.append(area);
+      map.append(area);
+    }
+  } catch {
+
   }
- 
-  block.appendChild(image);
-  block.appendChild(map);
+  try {
+    block.appendChild(image);
+    block.appendChild(map);
+  } catch {
+
+  }
 
 }
 
--- blocks/map/map.js
+++ blocks/map/map.js
@@ -8,40 +8,44 @@ export default function decorate(block) {
   image.src = 'https://i.ibb.co/S393wpr/map.png';
   image.alt = 'Image Description';
   image.useMap = '#myMap';
-  
+
   const area1 = document.createElement('area');
   area1.shape = 'circle';
   area1.coords = '200,200,90';
-  area1.addEventListener('click', () => {
-    alert('Clicked on Area 1');
-  });
-  area1.style.borderBlockColor = 'red';
-area1.href="#"
-  
-  const area2 = document.createElement('area');
-  area2.shape = 'circle';
-  area2.coords = '780,780,140';
-  area2.addEventListener('click', () => {
-    alert('Clicked on Area 2');
-  });
-  area2.style.backgroundColor = 'blue';
-area2.href="#"
-  
-  // image.addEventListener('click', () => {
-  //   console.log("I'm he")
-  // });
-  map.append(area1);
-  map.append(area2);
-  
+  try {
+    area1.addEventListener('click', () => {
+      alert('Clicked on Area 1');
+    });
+    area1.style.borderBlockColor = 'red';
+    area1.href = "#"
+
+    const area2 = document.createElement('area');
+    area2.shape = 'circle';
+    area2.coords = '780,780,140';
+    area2.addEventListener('click', () => {
+      alert('Clicked on Area 2');
+    });
+    area2.style.backgroundColor = 'blue';
+    area2.href = "#"
+
+    // image.addEventListener('click', () => {
+    //   console.log("I'm he")
+    // });
+    map.append(area1);
+    map.append(area2);
+  } catch {
+
+  }
+
   // Append the image and map to the document
- // block.appendChild(image);
+  // block.appendChild(image);
   //block.appendChild(map);
-  
+
 }
-  
-  //block.append(image);
-  //block.append(map);
-  //block.append(map);
+
+//block.append(image);
+//block.append(map);
+//block.append(map);
 
 
 
