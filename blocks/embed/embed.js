bd10032e308e8a2076df81abd3a1492a9b89ac27
SamineHadadi <samine.hadadi@gmail.com>
2024-11-30 17:06:08 +1100

    YT embed

---
+++ blocks/embed/embed.js
@@ -0,0 +1,10 @@
+import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+
+export default function decorate(block) {
+  const iframe = document.createElement('iframe');
+  iframe.width = '100%';
+  iframe.height = '600';
+  iframe.src =new URL(block.textContent);
+  block.textContent ='';
+  block.append(iframe);
+}
